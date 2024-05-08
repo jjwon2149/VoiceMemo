937ec5b31a65ebeb3d47288e94f2c0c8ee943e54
jjwon2149 <jjwon2149@gmail.com>
2024-05-08 14:43:52 +0900

    feat: add CustomNavigation Bar

--- voiceMemo/Component/NavigationBar/NavigationBtnType.swift
+++ voiceMemo/Component/NavigationBar/NavigationBtnType.swift
@@ -5,6 +5,9 @@
 
 import Foundation
 
-enum NavigationBtnType {
-  
+enum NavigationBtnType: String {
+    case close
+    case edit = "편집"
+    case complete
+    case create = "생성"
 }
--- voiceMemo/Core/Extension/Date+Extensions.swift
+++ voiceMemo/Core/Extension/Date+Extensions.swift
@@ -6,5 +6,29 @@
 import Foundation
 
 extension Date {
-  
+    var foremattedTime: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "a hh:mm" //오전오후 시간
+        return formatter.string(from: self)
+    }
+    
+    var foremattedDay: String {
+        let now = Date()
+        let calendar = Calendar.current
+        
+        let nowStartOfDay = calendar.startOfDay(for: now)
+        let dateStartOfDay = calendar.startOfDay(for: self)
+        let numOfDayDifference = calendar.dateComponents([.day], from: nowStartOfDay, to: dateStartOfDay).day!
+        
+        if numOfDayDifference == 0 {
+            return "오늘"
+        } else {
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "ko_KR")
+            formatter.dateFormat = "M월 d일 E요일"
+            return formatter.string(from: self)
+        }
+    }
+    
 }
